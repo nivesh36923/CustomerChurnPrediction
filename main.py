2fb4ce6c68df9e46e00b38043946b9b46b4200fa
nivesh36923 <niveshofficia@gmail.com>
2025-03-10 16:45:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import streamlit as st
-import pandas as pd
-st.title('Customer Churn Prediction')
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+import xgboost as xgb
+from sklearn.metrics import accuracy_score, classification_report
 
+
+st.title('Customer Churn Prediction')
 st.write('Hello world!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/CustomerChurnPrediction/master/churn-bigml-20.csv')
+
+df_20 = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/CustomerChurnPrediction/master/churn-bigml-20.csv')
+df_80 = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/CustomerChurnPrediction/master/churn-bigml-80.csv')
+
+df = pd.concat([df_20,df_80])
+le = LabelEncoder()
+df = df.reset_index()
+
+with st.expander('Data'):
+  df
+df['State'] = le.fit_transform(df['State'])
 
 with st.expander('Data'):
   df
