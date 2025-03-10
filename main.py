e98d00058b7f7330d676eccd03aa49f88eae5470
nivesh36923 <niveshofficia@gmail.com>
2025-03-10 16:42:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ st.title('Customer Churn Prediction')
 
 st.write('Hello world!')
 
-df = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/CustomerChurnPrediction/master/Churn-bigml-20.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/CustomerChurnPrediction/master/churn-bigml-20.csv')
 
 with st.expander('Data'):
   df
