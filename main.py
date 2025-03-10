8b9974db833e3a86d2cfdf43f54cd39abb51b9b0
nivesh36923 <niveshofficia@gmail.com>
2025-03-10 16:39:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ import pandas as pd
 st.title('Customer Churn Prediction')
 
 st.write('Hello world!')
+
+df = pd.read_csv('https://raw.githubusercontent.com/nivesh36923/nd-ml/master/stock_data.csv')
+
+with st.expander('Data'):
+  df
