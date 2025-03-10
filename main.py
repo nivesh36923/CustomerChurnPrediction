5bb2bd3f8bfa9e73bbc31ab1fbbc1c375e44d2ac
nivesh36923 <niveshofficia@gmail.com>
2025-03-10 18:43:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -49,7 +49,17 @@ st.info('process done')
 
 
 st.selectbox('**Select State**',dfcopy['State'])
-number = st.number_input("**Enter an integer:**", value=3.452)
+Account_length = st.number_input("**Account length:**", value=3.452)
+Area_code = st.number_input("Area code**", value=3.452)
+Total_day_minutes = st.number_input("**Total day minutes:**", value=3.452)
+Total_day_calls = st.number_input("**Total day calls:**", value=3.452)
+Total_day_charge = st.number_input("**Total day charge:**", value=3.452)
+Total_eve_minutes = st.number_input("**Total eve minutes:**", value=3.452)
+Total_eve_calls = st.number_input("**Total eve calls:**", value=3.452)
+Total_night_minutes = st.number_input("**Total night minutes:**", value=3.452)
+Total_night_calls = st.number_input("**Total night calls:**", value=3.452)
+Total_night_charge = st.number_input("**Total night charge:**", value=3.452)
+Customer_service_calls = st.number_input("**TCustomer service calls:**", value=3.452)
 
 st.write(number)
 
