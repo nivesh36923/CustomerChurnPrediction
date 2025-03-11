edec0dbe9cfccb19c7289c4b905a2954f45a0d73
nivesh36923 <niveshofficia@gmail.com>
2025-03-11 12:03:35 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -3,5 +3,26 @@ import pandas as pd
 
 st.title('Customer Churn Predictor')
 
-st.write('fil the data')
+st.write('**fil the data**')
 
+Account_length = st.number_input("**Account length:**", value=3.452)
+Area_code = st.number_input("Area code**", value=3.452)
+Total_day_minutes = st.number_input("**Total day minutes:**", value=3.452)
+Total_day_calls = st.number_input("**Total day calls:**", value=3.452)
+Total_day_charge = st.number_input("**Total day charge:**", value=3.452)
+Total_eve_minutes = st.number_input("**Total eve minutes:**", value=3.452)
+Total_eve_calls = st.number_input("**Total eve calls:**", value=3.452)
+Total_eve_charge = st.number_input("**Total eve charge:**", value=3.452)
+Total_night_minutes = st.number_input("**Total night minutes:**", value=3.452)
+Total_night_calls = st.number_input("**Total night calls:**", value=3.452)
+Total_night_charge = st.number_input("**Total night charge:**", value=3.452)
+Customer_service_calls = st.number_input("**TCustomer service calls:**", value=3.452)
+
+if agree == True:
+  st.write('welcome')
+
+
+  if st.button("Submit"):
+    st.write('success')
+    
+  
