50744e1840585d1ec78cf8578f86a3a61e5aa485
nivesh36923 <niveshofficia@gmail.com>
2025-03-11 12:30:25 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -5,6 +5,10 @@ st.title('Customer Churn Predictor')
 
 st.write('**fil the data**')
 
+loaded_model = joblib.load("xgboost_model.joblib")
+
+
+
 Account_length = st.number_input("**Account length:**", value=3.452)
 Area_code = st.number_input("**Area code**", value=3.452)
 Total_day_minutes = st.number_input("**Total day minutes:**", value=3.452)
@@ -22,9 +26,13 @@ if agree == True:
   st.write('welcome')
 
 
+
+  
   if st.button("Submit"):
+    x_test = np.array([Account_length,Area_code,Total_day_minutes,Total_day_calls,Total_day_charge,Total_eve_minutes,Total_eve_calls,Total_eve_charge,Total_night_minutes,Total_night_calls,Total_night_charge,Customer_service_calls])
+    y_pred = loaded_model.predict(X_test)
     st.write('success')
-    st.write(Account_length)
+    st.write(y_pred)
     
 
 
