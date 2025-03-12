270632d9966f2a9a00c186fb6bdb1d79b9d7b0d4
nivesh36923 <niveshofficia@gmail.com>
2025-03-12 11:14:29 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -36,7 +36,8 @@ if agree == True:
 
 
   if st.button("Submit"):
-    X_test = np.array([Account_length,Area_code,Total_day_minutes,Total_day_calls,Total_day_charge,Total_eve_minutes,Total_eve_calls,Total_eve_charge,Total_night_minutes,Total_night_calls,Total_night_charge,Customer_service_calls])
+    X = np.array([Account_length,Area_code,Total_day_minutes,Total_day_calls,Total_day_charge,Total_eve_minutes,Total_eve_calls,Total_eve_charge,Total_night_minutes,Total_night_calls,Total_night_charge,Customer_service_calls])
+    X_test = X.reshape(1, -1)
     y_pred = loaded_model.predict(X_test)
     
     st.write('y_pred : ',y_pred)
